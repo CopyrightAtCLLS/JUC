cd4f3ac634e631269d2f1189752b8ede6b9651d5
CopyrightAtCLLS <939085525@qq.com>
2021-01-23 16:47:31 +0800

    modify

--- src/main/java/synchronizedDemo/SecureStaticSale.java
+++ src/main/java/synchronizedDemo/SecureStaticSale.java
@@ -2,15 +2,21 @@ package synchronizedDemo;
 
 public class SecureStaticSale {
     public static void main(String[] args) {
+
         new Thread(()->{StaticTicket.sale();},"A").start();
         new Thread(()->{StaticTicket.sale();},"B").start();
+        //无论是通过类还是对象调用 静态同步方法，锁的目的都能达到
+        StaticTicket staticTicket=new StaticTicket();
+        new Thread(()->{staticTicket.sale();},"C").start();
     }
 }
 class StaticTicket{
-    private static int ticket=10;
+    private static int ticket=500;
 
     public static void sale(){
         while (true) {
+            //静态synchronized代码块的锁对象必须是 类.class
+            //如果是静态方法，锁对象也是class
             synchronized (StaticTicket.class) {
                 //同步代码块必须放在while内部，否则成为了其余线程不可能参与抢票
                 if (ticket > 0) {
