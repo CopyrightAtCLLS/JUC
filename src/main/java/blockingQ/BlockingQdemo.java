810faf443c41d9c246acbabd9995aae3e45a0666
CopyrightAtCLLS <939085525@qq.com>
2021-01-24 16:47:44 +0800

    n

--- src/main/java/blockingQ/BlockingQdemo.java
+++ src/main/java/blockingQ/BlockingQdemo.java
@@ -2,12 +2,13 @@ package blockingQ;
 
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 public class BlockingQdemo {
-    public static void main(String[] args) {
-        test2();
+    public static void main(String[] args) throws InterruptedException {
+        test4();
     }
-    //add和remove方法会抛出异常
+    //add,remove,element方法会抛出异常
     public static void test1(){
         BlockingQueue queue=new ArrayBlockingQueue<>(2);
         System.out.println(queue.add("a"));
@@ -19,7 +20,7 @@ public class BlockingQdemo {
 //        System.out.println(queue.remove());
     }
 
-    //offer和poll不抛出异常，而是返回false和null
+    //offer,poll,peek不抛出异常，而是返回false和null
     public static void test2(){
         BlockingQueue queue=new ArrayBlockingQueue<>(2);
         System.out.println(queue.offer("a"));
@@ -29,6 +30,28 @@ public class BlockingQdemo {
         System.out.println(queue.poll());
         System.out.println(queue.poll());
         System.out.println(queue.poll());
+    }
 
+    //put和take会阻塞
+    public static void test3() throws InterruptedException {
+        BlockingQueue queue=new ArrayBlockingQueue(2);
+        queue.put(1);
+        queue.put(2);
+//        queue.put(3);
+        System.out.println(queue.take());
+        System.out.println(queue.take());
+//        System.out.println(queue.take());
+    }
+
+    //offer和poll可以设置超时时间
+    public static void test4() throws InterruptedException {
+        BlockingQueue queue=new ArrayBlockingQueue<>(2);
+        System.out.println(queue.offer("a"));
+        System.out.println(queue.offer(2));
+        System.out.println(queue.offer("a",2, TimeUnit.SECONDS));
+
+        System.out.println(queue.poll());
+        System.out.println(queue.poll());
+        System.out.println(queue.poll(2,TimeUnit.SECONDS));
     }
 }
