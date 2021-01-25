20e64dff01e7e949b66fb9a994769e011d690d31
CopyrightAtCLLS <939085525@qq.com>
2021-01-25 16:05:10 +0800

    complete

--- src/main/java/jmm/InvisibilityDemo.java
+++ src/main/java/jmm/InvisibilityDemo.java
@@ -1,4 +1,15 @@
 package jmm;
 
+import java.util.concurrent.TimeUnit;
+
 public class InvisibilityDemo {
+    private static int num=0;
+    public static void main(String[] args) throws InterruptedException {
+        new Thread(()->{while(num==0);}).start();
+
+        TimeUnit.MILLISECONDS.sleep(1);
+
+        num=1;
+
+    }
 }
