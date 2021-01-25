5096d2dc368714e7e56db5c2080e33a077000fe4
CopyrightAtCLLS <939085525@qq.com>
2021-01-25 17:27:59 +0800

    invisibility solution

--- src/main/java/jmm/visibility/VisibilityDemo.java
+++ src/main/java/jmm/visibility/VisibilityDemo.java
@@ -1,4 +1,15 @@
 package jmm.visibility;
 
+import java.util.concurrent.TimeUnit;
+
 public class VisibilityDemo {
+    private volatile static int num=0;
+    public static void main(String[] args) throws InterruptedException {
+        new Thread(()->{while(num==0);}).start();
+
+        TimeUnit.MILLISECONDS.sleep(1);
+
+        num=1;
+
+    }
 }
