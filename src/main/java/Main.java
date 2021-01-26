50eaafcf9dd3440885f0d01ae32309a33cf4f589
CopyrightAtCLLS <939085525@qq.com>
2021-01-26 14:16:52 +0800

    main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,16 +3,5 @@ import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        System.out.println("JUC");
-
-        new Thread(()->{
-            System.out.println("A");
-        }).start();
-        new Thread(()->{
-            System.out.println("B");
-        }).start();
-        new Thread(()->{
-            System.out.println("C");
-        }).start();
     }
 }
