5aad8a59fcf1b0d6bf5d9947a0050779bcb46945
CopyrightAtCLLS <939085525@qq.com>
2021-03-31 19:11:27 +0800

    FutureTask demo

---
+++ src/main/java/Callable/demo2/FutureTaskTest.java
@@ -0,0 +1,28 @@
+package Callable.demo2;
+
+import java.util.concurrent.*;
+
+public class FutureTaskTest {
+    public static void main(String[] args) throws ExecutionException, InterruptedException, TimeoutException {
+        ExecutorService service = Executors.newFixedThreadPool(10);
+        //Callable
+        FutureTask futureTask = new FutureTask(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                TimeUnit.MILLISECONDS.sleep(1000);
+                return "The return value of Callable";
+            }
+        });
+        service.submit(futureTask);
+        System.out.println(futureTask.get(1200, TimeUnit.MILLISECONDS) + "\n");
+
+        final String tmp = "returnValue";
+        futureTask = new FutureTask(() -> {
+            System.out.println("Runnable Thread...");
+        }, tmp);
+        service.submit(futureTask);
+        System.out.println(futureTask.get());
+
+        service.shutdown();
+    }
+}
