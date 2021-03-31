a216491df80a4718e4d32061afeb2e0e597d2942
CopyrightAtCLLS <939085525@qq.com>
2021-03-31 18:17:32 +0800

    Add usage of Future

--- src/main/java/Callable/demo1/Intro.java
+++ src/main/java/Callable/demo1/Intro.java
@@ -2,23 +2,31 @@ package Callable.demo1;
 
 import java.util.concurrent.*;
 
-public class Main {
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
+public class Intro {
+    public static void main(String[] args) throws ExecutionException, InterruptedException, TimeoutException {
         ExecutorService service = Executors.newFixedThreadPool(10);
+        //Runnable
         Future<?> runnableFuture = service.submit(new Runnable() {
             @Override
             public void run() {
                 System.out.println("This is Runnable, no return value!");
             }
         });
-        System.out.println(runnableFuture.get());
+        System.out.println(runnableFuture.get()+"\n");
+
+        //Callable
         Future<String> callableFuture = service.submit(new Callable<String>() {
             @Override
             public String call() throws Exception {
+                TimeUnit.MILLISECONDS.sleep(1000);
                 return "This is return value of Callalbe!";
             }
         });
-        System.out.println(callableFuture.get());
+        //get是阻塞方法
+        System.out.println(callableFuture.get(1200, TimeUnit.MILLISECONDS));
+
+
+        System.out.println("Finished!");
         service.shutdown();
     }
 }
